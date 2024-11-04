f93cb3ea918fe85b37ca524514a37332aa08abf3
Pep Cots <pec@milestone.dk>
2024-11-04 21:37:48 +0100

    Rule of 5 applied to TreeExt

--- glut_simple_example/glut_simple_example/TreeExt.h
+++ glut_simple_example/glut_simple_example/TreeExt.h
@@ -17,6 +17,27 @@ public:
 	auto& root() const { return _parent ? _parent->root() : *this; }
 	bool isRoot() const { return !_parent; }
 
+	TreeExt() = default;
+
+	TreeExt(const TreeExt& other) : _children(other._children) {
+		for (auto& child : _children) child._parent = static_cast<T*>(this);
+	}
+
+	TreeExt& operator=(const TreeExt& other) {
+		_children = other._children;
+		for (auto& child : _children) child._parent = static_cast<T*>(this);
+		return *this;
+	}
+
+	TreeExt(TreeExt&& other) noexcept : _parent(other._parent), _children(std::move(other._children)) {
+		for (auto& child : _children) child._parent = static_cast<T*>(this);
+		other._parent = nullptr;
+	}
+
+	TreeExt& operator=(TreeExt&& other) noexcept = delete;
+
+	virtual ~TreeExt() = default;
+
 	template <typename ...Args>
 	auto& emplaceChild(Args&&... args) {
 		_children.emplace_back(std::forward<Args>(args)...);
