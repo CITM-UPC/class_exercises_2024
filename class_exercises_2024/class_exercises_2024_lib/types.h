f8c015cb4a4086077cfb843083ebf5942aa18b67
Pep Cots <pec@milestone.dk>
2024-11-19 00:56:14 +0100

    Basic algebra types defined with transform operators

--- class_exercises_2024/class_exercises_2024_lib/types.h
+++ class_exercises_2024/class_exercises_2024_lib/types.h
@@ -18,3 +18,28 @@ namespace Colors {
 	const color4 Green( 0, 255, 0, 255);
 	const color4 Blue( 0, 0, 255, 255);
 };
+
+struct Point {
+	vec3 p;
+	Point(const vec3& v3) : p(v3) {}
+};
+
+inline Point operator*(const mat4& m, const Point& p) {
+	const auto v3 =  m * vec4(p.p, 1);
+	return Point(vec3(v3) / v3.w);
+}
+
+struct Vector {
+	vec3 v;
+	Vector(const vec3& v3) : v(v3) {}
+};
+
+inline Vector operator*(const mat4& m, const Vector& v) { return Vector(vec3(m * vec4(v.v, 0))); }
+
+struct Plane {
+	Vector normal;
+	Point point;
+	Plane(const Vector& normal, const Point& point) : normal(normal), point(point) {}
+};
+
+inline Plane operator*(const mat4& m, const Plane& plane) { return Plane(m * plane.normal, m * plane.point); }
